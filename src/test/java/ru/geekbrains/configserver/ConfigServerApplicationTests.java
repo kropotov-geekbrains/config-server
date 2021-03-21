8bec30b44a5ffe41240d833357aa8e26295f8c70
Artem Kropotov <AAKropotov@alfabank.ru>
2021-03-21 14:55:49 +0300

    del tests

--- src/test/java/ru/geekbrains/configserver/ConfigServerApplicationTests.java
+++ src/test/java/ru/geekbrains/configserver/ConfigServerApplicationTests.java
@@ -1,13 +1,13 @@
 package ru.geekbrains.configserver;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ConfigServerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class ConfigServerApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
